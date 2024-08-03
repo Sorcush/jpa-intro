69d5bb320327bbb763e5b795669c99e3cb689f98
Andrey Sloutsman <sorcus@gmail.com>
2024-08-03 15:26:20 -0400

    Split to vendor specific flyway migrations.

--- src/main/java/com/example/jpaintro/entity/Book.java
+++ src/main/java/com/example/jpaintro/entity/Book.java
@@ -14,7 +14,7 @@ import lombok.*;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
