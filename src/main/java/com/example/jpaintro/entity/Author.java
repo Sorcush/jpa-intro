7b1ab6d2ebdb22764a84b5d39490896e149548f6
Andrey Sloutsman <sorcus@gmail.com>
2024-08-02 22:28:37 -0400

    Setup flyway. Add Author table.

---
+++ src/main/java/com/example/jpaintro/entity/Author.java
@@ -0,0 +1,24 @@
+package com.example.jpaintro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@ToString
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+}
